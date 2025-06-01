cc7d81c0f1b349309a436ffb644957bf3f3e213d
Arteks718 <artexbro.official@gmail.com>
2025-06-01 13:05:36 +0300

    Add adaptive in Navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,6 +1,8 @@
 import { auth, signIn, signOut } from "@/auth";
+import { BadgePlus, LogOut } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
+import { Avatar, AvatarImage, AvatarFallback } from "./ui/avatar";
 
 const Navbar = async () => {
 	const session = await auth();
@@ -16,29 +18,40 @@ const Navbar = async () => {
 					{session && session?.user ? (
 						<>
 							<Link href='/startup/create'>
-								<span>Create</span>
+								<span className='max-sm:hidden'>Create</span>
+								<BadgePlus className='size-6 sm:hidden' />
 							</Link>
 							<form
+								className="inline-flex items-center"
 								action={async () => {
-                  "use server"
+									"use server";
 									await signOut({ redirectTo: "/" });
 								}}
 							>
-								<button type="submit">Logout</button>
+								<button type='submit'>
+									<span className='max-sm:hidden'>Logout</span>
+									<LogOut className='size-6 sm:hidden text-red-500' />
+								</button>
 							</form>
 							<Link href={`/user/${session?.id}`}>
-								<span>{session?.user?.name}</span>
+								<Avatar className='size-10'>
+									<AvatarImage
+										src={session?.user?.image || ""}
+										alt={session?.user?.name || "User Avatar"}
+									/>
+									<AvatarFallback>AV</AvatarFallback>
+								</Avatar>
 							</Link>
 						</>
 					) : (
 						<>
 							<form
 								action={async () => {
-                  "use server"
-									await signIn('github');
+									"use server";
+									await signIn("github");
 								}}
 							>
-								<button type="submit">Login</button>
+								<button type='submit'>Login</button>
 							</form>
 						</>
 					)}
